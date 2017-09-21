a3ef769829d4d4e952267447ac05b02590bd7cc8
Nemanja Majstorović <nemanjama@telekom.yu>
2017-09-21 15:17:10 +0200

    Izmene na aplikaciji - objasnjenje i novi build

--- shortener/src/main/java/com/nemanja/shortener/ShortenerApplication.java
+++ shortener/src/main/java/com/nemanja/shortener/ShortenerApplication.java
@@ -1,10 +1,11 @@
 package com.nemanja.shortener;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShortenerApplication  {
+public class ShortenerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortenerApplication.class, args);
